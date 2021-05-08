f9ebf46897044ea803af8ac51f438719a8cd7573
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-05-08 19:59:42 +0600

    Create form

--- app/Forms/TruckForm.php
+++ app/Forms/TruckForm.php
@@ -3,14 +3,25 @@
 namespace App\Forms;
 
 use Kris\LaravelFormBuilder\Form;
+use Kris\LaravelFormBuilder\Field;
 
 class TruckForm extends Form
 {
     public function buildForm()
     {
         $this
-            ->add('name', 'text')
-            ->add('lyrics', 'textarea')
-            ->add('publish', 'checkbox');
+            ->add('Auto brand', Field::SELECT, [
+                'rules' => 'required',
+                'choices' => ['Volvo' => 'Volvo', 'VAZ' => 'VAZ', 'Mercedes' => 'Mercedes', 'GAZ' => 'GAZ'],
+                'empty_value' => '=== Select one ==='
+            ])
+            ->add('Year of Manufacture', Field::NUMBER, [
+                'rules' => 'required'
+            ])
+            ->add('Owner‘s Name and Surname', Field::TEXT, [
+                'rules' => 'required'
+            ])
+            ->add('Number of owners', 'number')
+            ->add('Comments', 'textarea');
     }
 }
