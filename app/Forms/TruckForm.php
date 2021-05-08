18f78307e7de1a532143a9423cbcf98cf7f84e3e
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-05-08 20:05:06 +0600

    Create form update

--- app/Forms/TruckForm.php
+++ app/Forms/TruckForm.php
@@ -13,10 +13,10 @@ class TruckForm extends Form
             ->add('Auto brand', Field::SELECT, [
                 'rules' => 'required',
                 'choices' => ['Volvo' => 'Volvo', 'VAZ' => 'VAZ', 'Mercedes' => 'Mercedes', 'GAZ' => 'GAZ'],
-                'empty_value' => '=== Select one ==='
+                'empty_value' => '=== Select brand ==='
             ])
             ->add('Year of Manufacture', Field::NUMBER, [
-                'rules' => 'required'
+                'rules' => 'required|min:1900|max:'.date('y')
             ])
             ->add('Owner‘s Name and Surname', Field::TEXT, [
                 'rules' => 'required'
